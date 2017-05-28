fc47bef08fabb3de4b4af2035203bab3ff0bdb95
kylewstewart <kyle.w.stewart@mac.com>
2017-05-27 23:37:04 -0400

    bed time

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,17 @@ class Student < ApplicationRecord
   has_many :school_prefs
   has_many :schools, through: :school_prefs
 
+  def self.distribution
+    students = Students.all
+    dist = {}
+    students.each do |student|
+      byebug
+      student.school_prefs
+
+    end
+
+  end
+
+
+
 end
